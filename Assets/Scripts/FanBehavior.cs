c205f4e63a6af275dadbe380ddf5cd04de6a81f4
Yasser Makram <yasser@barm.ag>
2017-10-11 12:21:04 +0200

    Fan prefab

---
+++ Assets/Scripts/FanBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanBehavior : MonoBehaviour {
+    [Range(1, 50)]
+    public int force = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("Fan Hit!");
+
+        if (collider.CompareTag("Throwable"))
+        {
+            collider.gameObject.GetComponent<Rigidbody>().AddForce(-transform.forward * force, ForceMode.Acceleration);
+        }
+    }
+}
