c0bb0920e9b4e8c3a9b33a10a5bbb4c6d4970997
Yasser Makram <yasser@barm.ag>
2017-10-11 17:00:51 +0200

    Implement movable behavior for rube prefabs based on steamvr interactable

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -8,6 +8,13 @@ using Valve.VR.InteractionSystem;
 [RequireComponent(typeof(Rigidbody))]
 
 public class Movable : MonoBehaviour {
+    [EnumFlags]
+    [Tooltip("The flags used to attach this object to the hand.")]
+    public Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.DetachFromOtherHand;
+
+    [Tooltip("Name of the attachment transform under in the hand's hierarchy which the object should should snap to.")]
+    public string attachmentPoint;
+
     private bool attached;
     private float attachTime;
     private Vector3 attachPosition;
@@ -26,8 +33,24 @@ public class Movable : MonoBehaviour {
 		
 	}
 
+    private void OnHandHoverBegin(Hand hand)
+    {
+        Debug.Log("Hand hover!");
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        //Trigger got pressed
+        if (hand.GetStandardInteractionButtonDown())
+        {
+            hand.AttachObject(gameObject, attachmentFlags, attachmentPoint);
+            ControllerButtonHints.HideButtonHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+        }
+    }
+
     private void OnAttachedToHand(Hand hand)
     {
+        Debug.Log("Hand attached!");
         attached = true;
 
         onPickUp.Invoke();
@@ -57,5 +80,40 @@ public class Movable : MonoBehaviour {
         rb.interpolation = RigidbodyInterpolation.Interpolate;
 
         Vector3 position = Vector3.zero;
+    }
+
+    private void HandAttachedUpdate(Hand hand)
+    {
+        Debug.Log("Attach update!");
+        //Trigger got released
+        if (!hand.GetStandardInteractionButton())
+        {
+            // Detach ourselves late in the frame.
+            // This is so that any vehicles the player is attached to
+            // have a chance to finish updating themselves.
+            // If we detach now, our position could be behind what it
+            // will be at the end of the frame, and the object may appear
+            // to teleport behind the hand when the player releases it.
+            StartCoroutine(LateDetach(hand));
+        }
+    }
+
+    private void OnHandFocusAcquired(Hand hand)
+    {
+        Debug.Log("Hand focused");
+        gameObject.SetActive(true);
+    }
+
+    private void OnHandFocusLost(Hand hand)
+    {
+        Debug.Log("Hand lost focus");
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator LateDetach(Hand hand)
+    {
+        yield return new WaitForEndOfFrame();
+
+        hand.DetachObject(gameObject, false);
     }
 }
