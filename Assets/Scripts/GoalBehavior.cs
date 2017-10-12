bd6eeac19101df345a23e6919928b46316fc73cd
Yasser Makram <yasser@barm.ag>
2017-10-12 18:06:36 +0200

    Implement goal behavior, if hit with all objectives collected, load next level

---
+++ Assets/Scripts/GoalBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBehavior : MonoBehaviour {
+
+    public string nextLevel;
+    public GameObject objectivesRoot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Throwable"))
+        {
+            Debug.Log("Goal hit!");
+            foreach (Transform t in objectivesRoot.transform)
+            {
+                var c = t.gameObject.GetComponent<Collectible>();
+                if (c != null)
+                {
+                    if (!c.isCollected)
+                    {
+                        return;
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(nextLevel))
+            {
+                SteamVR_LoadLevel.Begin(nextLevel);
+            }
+        }
+    }
+}
